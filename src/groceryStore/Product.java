25d8c6322d27fbdeb3fcd3deb1a8c126e9b568d3
master-1000 <87445964+master-1000@users.noreply.github.com>
2023-04-16 21:07:00 -0300

    Add files via upload
    atualização 1.0

---
+++ src/groceryStore/Product.java
@@ -0,0 +1,49 @@
+package groceryStore;
+
+public class Product {
+	  
+	private String name;
+	private double price;
+	private String barcode;
+	private int stockQuantity;        
+	
+	public Product(String name, double price, String barcode, int stockQuantity) {
+		this.name = name;
+		this.price = price;
+		this.barcode = barcode;
+		this.stockQuantity = stockQuantity;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public String getBarcode() {
+		return barcode;
+	}
+	
+	public int getStockQuantity() {
+		return stockQuantity;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+	
+	public void setStockQuantity(int stockQuantity) {
+		this.stockQuantity = stockQuantity;
+	}
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author carlos.leal
+ *
+ */
+module groceryStore {
+}
