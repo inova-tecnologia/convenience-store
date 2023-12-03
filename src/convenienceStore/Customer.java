0842e64716886140b0c191638e76d3dc59c17c49
Carlos L <87445964+inova-tecnologia@users.noreply.github.com>
2023-12-03 14:33:51 -0300

    clean code adjustments

--- src/convenienceStore/Customer.java
+++ src/convenienceStore/Customer.java
@@ -2,10 +2,8 @@ package convenienceStore;
 
 import java.util.Date;
 
-public class Customer extends Person{
-	 
+public class Customer extends Person{	 
 	public Customer(int socialSecurityNumber, String name, Date birthDate, Address address, String phoneNumber, String mail) {
 		super(socialSecurityNumber, name, birthDate, address, phoneNumber, mail);
-	}
-	
+	}	
 }
